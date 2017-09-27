e546f5e9f16a4c5f9109b983c47e2fbf1e8ea8b7
npes <npesdk@gmail.com>
2017-09-27 14:37:19 +0200

    Arrays exercise C#

--- Enums/Program.cs
+++ Enums/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 namespace Enums
 {
@@ -10,6 +10,26 @@ namespace Enums
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your favourite car?");
+
+            var answer = Console.ReadLine();
+            if (answer.ToLower() == CarType.Skoda.ToString().ToLower()) //to lower because user might use small letters
+            {
+                Console.WriteLine("Me too, you rock!");
+            }
+            else
+            {
+                Console.WriteLine("Buy a " + CarType.Skoda);
+            }
+            Console.ReadLine();
         }
+    }
+    public enum CarType
+    {
+        Toyota,
+        Honda,
+        Ford,
+        Skoda,
+        Audi
     }
 }
